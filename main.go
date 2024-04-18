95031bab16d7f89e674aa3d2c73a0870b44bc26d
Nitesh000 <ntudu040@gmail.com>
2024-04-19 00:07:17 +0530

    feat: add a repl for monkey lang

--- main.go
+++ main.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"Nitesh000/monkey_preter/repl"
+	"fmt"
+	"os"
+	"os/user"
+)
 
 func main() {
-	fmt.Print("Hello, world!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n", user.Username)
+	fmt.Printf("Fell free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
